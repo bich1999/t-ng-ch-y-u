854e472db086aaa386d05e4d8353a2a3625eedc8
Trần Xuân Bích <tranxuanbich@MacBook-Pro-cua-Tran.local>
2022-10-17 20:41:34 +0900

    upload source code

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
+    introDesc: `Em có điều này muốn hỏi chị, chị nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'OK',
+    title: 'Chị có Thích em không ? 😙',
+    desc: 'Chị không trả lời hoặc thoát ra, tức là Chị thích Em đó nha :> ',
+    btnYes: 'Có, Chị  thật sự thích em 💖💖💖',
+    btnNo: '💔 Em đang mơ à bé à ! 💔 ',
+    question:'Nói cho Em biết đi vì sao Chị thích em 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì thấy mái tóc của em đẹp á.... Mình hẹn hò nhau nha 😊😊',
+    mess: 'Em biết mờ, Em cũng thích Chị lắm 🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho em nha. 💬💬',
+    btnAccept: 'Mơ đi cưng 💖',
+    messLink: 'https://m.me/beng.tran.99'
+}
